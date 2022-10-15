21b2411743de12712ab510a235895f45ef98b69e
Zaixx <ivalasojarazo@gmail.com>
2022-10-15 15:31:39 -0300

    Use Slice

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	confName := "Go Conference"
 	const confTickets = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("ConfTickets is %T, remainingTickets is %T, conferenceNames is %T\n", confName, confTickets, remainingTickets)
 
@@ -13,14 +14,25 @@ func main() {
 	fmt.Printf("We have a total of %v out of %v\n", remainingTickets, confTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	for {
+		var userName string
+		var userTickets uint
 
-	fmt.Println("Enter your userName: ")
-	fmt.Scan(&userName)
+		fmt.Println("Enter your userName: ")
+		fmt.Scan(&userName)
 
-	fmt.Println("How many tickets? ")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets? ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+		bookings = append(bookings, userName)
+
+		remainingTickets = remainingTickets - userTickets
+
+		fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+		fmt.Printf("%v tickets remaining for the %v\n", remainingTickets, confName)
+
+		fmt.Printf("Current bookings are: %v\n", bookings)
+	}
+
 }
